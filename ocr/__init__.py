0273ca9421653c4119a5ff64fa9d75d0d5e03e3d
leon <jayli123@sina.com>
2018-11-13 16:29:08 +0800

    Add our

---
+++ ocr/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# coding:utf-8
+
+"""
+@Time: 2018/11/13 下午3:26
+@Author: leon
+@File: __init__.py.py
+@Software: PyCharm Community Edition
+"""
+
+if __name__ == "__main__":
+    print
+    ""
